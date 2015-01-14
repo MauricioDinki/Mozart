d83ba1fa637e9f5c6175ca16f43a8ea70ec83193
Mauricio Ivan Mejia Ramirez <mejiaramirez@outlook.com>
2015-01-13 20:41:33 -0600

    añadidos los serializers

---
+++ Works/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Work
+
+class WorkSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Work
+		fields = ('user', 'title', 'category')
