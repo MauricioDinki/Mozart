f3fcdeeeb61bbbc46fe31a08c4f1e05dd3876006
Mauricio Ivan Mejia Ramirez <mejiaramirez@outlook.com>
2015-01-19 19:12:25 -0600

    añadidos campos faltantes al formulario para modificar los datos

--- Profiles/forms.py
+++ Profiles/forms.py
@@ -7,10 +7,36 @@ from Profiles.models import days,months,type_of_users,sexuality,Mozart_User,Date
 
 class UserInformationForm(forms.Form):
 
+	day_of_birth = forms.ChoiceField(
+        error_messages={
+            'invalid_choice':('Selecciona una opcion valida'),
+            'required': default_error_messages['required']
+        },
+        required=True,
+        choices=days,
+    )
+
+	description = forms.CharField(
+		error_messages=default_error_messages,
+		max_length=200,
+		required=True,
+	)
+	facebook = forms.URLField(
+		error_messages=default_error_messages,
+		required=True,
+		max_length=30,
+	)
+
 	first_name = forms.CharField(
 		error_messages=default_error_messages,
 		max_length=30,
 		required=True,
+	)
+
+	google = forms.URLField(
+		error_messages=default_error_messages,
+		required=True,
+		max_length=30,
 	)
 
 	last_name = forms.CharField(
@@ -19,15 +45,13 @@ class UserInformationForm(forms.Form):
 		required=True,
 	)
 
-	description = forms.CharField(
-		error_messages=default_error_messages,
-		max_length=200,
-		required=True,
-	)
-
-	phone_number = forms.IntegerField(
-		error_messages=default_error_messages,
-		required=True,
+	month_of_birth = forms.ChoiceField(
+	    error_messages={
+	        'invalid_choice':('Selecciona una opcion valida'),
+	        'required': default_error_messages['required']
+	    },
+	    required=True,
+	    choices=months,
 	)
 
 	nationality = forms.ChoiceField(
@@ -38,9 +62,44 @@ class UserInformationForm(forms.Form):
         },
 		required=True,
 	)
+	
+	password = forms.CharField(
+	    error_messages=default_error_messages,
+	    max_length=20,
+	    required=True,
+	    widget=forms.PasswordInput(),
+	)
+
+	personal_homepage = forms.URLField(
+		error_messages=default_error_messages,
+		required=True,
+		max_length=30,
+	)
+
+	phone_number = forms.IntegerField(
+		error_messages=default_error_messages,
+		required=True,
+	)
 
 
+	twitter = forms.URLField(
+		error_messages=default_error_messages,
+		required=True,
+		max_length=30,
+	)
 
+	year_of_birth = forms.IntegerField(
+	    error_messages=default_error_messages,
+	    required=True,
+	    max_value=2015,
+	    min_value=1905,
+	)
 
+	youtube = forms.URLField(
+		error_messages=default_error_messages,
+		required=True,
+		max_length=30,
+	)
 
-   
+	def __init__(self, *args, **kwargs):
+		super(UserInformationForm, self).__init__(*args, **kwargs)
