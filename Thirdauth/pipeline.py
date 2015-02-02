d5ff4d5bf4c4adb871abc6b18b6115ba8dcb57e1
Maurico Ivan <mauriciodinki@gmail.com>
2015-02-02 00:41:46 -0600

    terminado el pipeline para guardar los usernames

--- Thirdauth/pipeline.py
+++ Thirdauth/pipeline.py
@@ -9,28 +9,15 @@ def save_extra_params(backend, details, response, uid, user, *args, **kwargs):
 	active_user = User.objects.get(username = kwargs['username'])
 	user_username = parameters['username']
 	if backend.__class__ is FacebookOAuth2:
-		social_names = SocialUserName(user = active_user)
-		social_names.facebook = user_username
-		social_names.save()
-	# elif backend.__class__ is TwitterOAuth:
-
-	# 	exist_socialusername = SocialUserName.objects.get(user = active_user)
-	# 	if exist_socialusername:
-	# 		exist_socialusername.twitter = '@%s'%(user_username)
-	# 		exist_socialusername.save()
-	# 	else:
-	# 		social_names = SocialUserName(user = active_user)
-	# 		social_names.twitter = '@%s'%(user_username)
-	# 		social_names.save()
-
-	# elif backend.__class__ is GoogleOAuth2:
-
-	# 	exist_socialusername = SocialUserName.objects.get(user = active_user)
-	# 	if exist_socialusername:
-	# 		exist_socialusername.google = user_username
-	# 		exist_socialusername.save()
-	# 	else:
-	# 		social_names = SocialUserName(user = active_user)
-	# 		social_names.google = user_username
-	# 		social_names.save()
+		socialusername = SocialUserName.objects.get(user = active_user)
+		socialusername.facebook = user_username
+		socialusername.save()
+	elif backend.__class__ is TwitterOAuth:
+		socialusername = SocialUserName.objects.get(user = active_user)
+		socialusername.twitter = '@%s'%(user_username)
+		socialusername.save()
+	elif backend.__class__ is GoogleOAuth2:
+		socialusername = SocialUserName.objects.get(user = active_user)
+		socialusername.google = user_username
+		socialusername.save()
 
