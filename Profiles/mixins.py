273940d8bdd8752e54317a0f4ab402b9afe5ad34
Maurico Ivan <mauriciodinki@gmail.com>
2015-02-08 14:05:03 -0600

    creado mixin para poner el request en los argumentos del form view

--- Profiles/mixins.py
+++ Profiles/mixins.py
@@ -1,1 +1,7 @@
 # -*- encoding: utf-8 -*-
+
+class RequestFormMixin(object):
+	def get_form_kwargs(self):
+		kwargs = super( RequestFormMixin,self).get_form_kwargs()
+		kwargs['request'] = self.request
+		return kwargs
