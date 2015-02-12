dd85999110a732d366e762176c2d9772540e7747
Alan Sanchez <sanchez2597asp@outlook.com>
2015-02-12 17:35:16 -0600

    Minificacion de bower_components

--- Mozart/static/app/js/services/peticionObras.js
+++ Mozart/static/app/js/services/peticionObras.js
@@ -12,7 +12,7 @@ app.service('peticionObras', ['$http',  function($http){
   this.get=function(fnOK,fnError, modo, categoria, autor, cantidad) {
     $http({
       method: 'GET',
-      url: 'http://development.com:8000/api/works'
+      url: 'http://mozart.com:8000/api/works'
     })
     .success(function(data, status, headers, config) {
       fnOK(data);
