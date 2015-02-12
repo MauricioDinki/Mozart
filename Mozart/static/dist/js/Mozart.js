099de5224f370193172a5652aa7f11885484bb75
Alan Sanchez <sanchez2597asp@outlook.com>
2015-02-11 22:18:05 -0600

    Agregados temĺates

--- Mozart/static/dist/js/Mozart.js
+++ Mozart/static/dist/js/Mozart.js
@@ -444,7 +444,7 @@ app.service('peticionObras', ['$http',  function($http){
   this.get=function(fnOK,fnError, modo, categoria, autor, cantidad) {
     $http({
       method: 'GET',
-      url: 'http://localhost:8000/api/works'
+      url: 'http://development.com:8000/api/works'
     })
     .success(function(data, status, headers, config) {
       fnOK(data);
