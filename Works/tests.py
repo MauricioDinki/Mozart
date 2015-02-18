c10cacc179b319c070fd0d2924dc0b8664cbed62
Maurico Ivan <mauriciodinki@gmail.com>
2015-02-17 18:35:43 -0600

    Añadidas pruebas a las vistas de url del modulo Works

--- Works/tests.py
+++ Works/tests.py
@@ -1,3 +1,22 @@
 # -*- encoding: utf-8 -*-
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse_lazy
+
+class WorkTets(TestCase):
+
+	# Pruebas Para las vistas
+	def test_views(self):
+		
+		# Subir Una Obra
+		estatus = self.client.get(reverse_lazy('create_work'))
+		self.assertEqual(estatus.status_code, 200)
+
+		# Explorar Obras
+		estatus = self.client.get(reverse_lazy('index'))
+		self.assertEqual(estatus.status_code, 200)
+
+		# Lista De Obras
+		estatus = self.client.get(reverse_lazy('work_list'))
+		self.assertEqual(estatus.status_code,200)
+
