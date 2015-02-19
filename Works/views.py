b289138a729de970eb107edc6c66055c0eb7600e
Maurico Ivan <mauriciodinki@gmail.com>
2015-02-18 21:59:18 -0600

    añadida vista que elimina obras

--- Works/views.py
+++ Works/views.py
@@ -2,32 +2,33 @@
 
 from .forms import UploadWorkForm,EditWorkForm
 from .models import Work
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse_lazy
 from django.http import JsonResponse
-from django.shortcuts import render,get_list_or_404,redirect
-from django.views.generic import ListView,DetailView,TemplateView,FormView,UpdateView
+from django.shortcuts import render,get_list_or_404,redirect,get_object_or_404
+from django.views.generic import ListView,DetailView,TemplateView,FormView,UpdateView,DeleteView
 from Profiles.mixins import RequestFormMixin
 from Thirdauth.mixins import AuthRedirectMixin, LoginRequiredMixin
 
 class EditWorkView(LoginRequiredMixin,UpdateView):
-	template_name = 'edit-work-form.html'
 	form_class = EditWorkForm
-	success_url =  reverse_lazy('work_list')
 	model = Work
-	slug_url_kwarg = 'slug'
 	slug_field = 'slug'
+	slug_url_kwarg = 'slug'
+	success_url =  reverse_lazy('work_list')
+	template_name = 'edit-work-form.html'
 
 	def get_object(self):
-		obj = self.model.objects.get(user = self.request.user, slug = self.kwargs.get(self.slug_url_kwarg, None))
+		obj = get_object_or_404(self.model, user = self.request.user, slug = self.kwargs.get(self.slug_url_kwarg, None))
 		return obj
 
 class HomeView(AuthRedirectMixin,TemplateView):
     template_name = "index.html"
 
 class UploadWorkView(RequestFormMixin,FormView):
-	template_name = 'subirobra.html'
 	form_class = UploadWorkForm
 	success_url =  reverse_lazy('create_work')
+	template_name = 'subirobra.html'
 
 
 	def form_valid(self,form):
@@ -37,8 +38,8 @@ class UploadWorkView(RequestFormMixin,FormView):
 		return super(UploadWorkView,self).form_valid(form)
 
 class WorkListView(TemplateView):
-    template_name = "explore.html"
     model = Work
+    template_name = "explore.html"
 
 # class WorkDetailView(DetailView):
 
@@ -55,6 +56,12 @@ class WorkListView(TemplateView):
 # 			'archive':self.object.archive.url,
 # 		}]
 # 		return JsonResponse(data,safe=False)
+
+@login_required(login_url='login')
+def DeleteWorkView(request,slug):
+	account_to_delete = get_object_or_404(Work,user = request.user, slug = slug)
+	account_to_delete.delete()
+	return redirect(reverse_lazy('work_list'))
 
 
 
