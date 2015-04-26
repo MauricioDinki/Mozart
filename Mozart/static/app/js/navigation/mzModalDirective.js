4852dd1aa5542def09d2ef6d02f55b4c41511d98
Alan Sanchez <sanchez2597asp@outlook.com>
2015-04-26 18:04:44 -0500

    Deleted modal directive from settings design

--- Mozart/static/app/js/navigation/mzModalDirective.js
+++ Mozart/static/app/js/navigation/mzModalDirective.js
@@ -3,19 +3,14 @@
 
   function mozartModalDirective() {
     return {
-      restrict: 'AEC',
+      restrict: 'E',
       scope: {
         show: '=',
         modalTitle: '@'
       },
       transclude: true,
-      link: function(scope, elem, attrs){
-        scope.hideModal = function() {
-          scope.show = false;
-        };
-      },
       template: '<div class="modal-dialog-shadow" ng-show="show">' +
-    '<div class="modal-dialog-container" off-click="hideModal()" off-click-if="show">' +
+    '<div class="modal-dialog-container">' +
       '<h2 ng-bind="modalTitle"></h2><hr/>' +
       '<div ng-transclude></div>' +
     '</div>' +
--- Mozart/static/app/js/navigation/welcomeViewDirective.js
+++ Mozart/static/app/js/navigation/welcomeViewDirective.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
  
-  function mozartWelcomeDirective($window, $log) {
+  function mozartWelcomeDirective($window) {
     return {
       restrict: 'A',
       scope: {
@@ -19,33 +19,27 @@
           .scroll(function (event){
             var sectionHeight = elem.innerHeight();
             var height = angular.element(event.target).scrollTop();
-            $log.log(height + ':' + sectionHeight);
             if(height < sectionHeight) {
               elem.css({'background-image':'url(' + images[0] + ')'});
-              $log.log('aa');
             }
             else if(height < 2*(sectionHeight)) {
               elem.css({'background-image':'url(' + images[1] + ')'});
-              $log.log('bb');
             }
             else if(height < 3*(sectionHeight)) {
               elem.css({'background-image':'url(' + images[2] + ')'});
-              $log.log('cc');
             }
             else if(height < 4*(sectionHeight)) {
               elem.css({'background-image':'url(' + images[3] + ')'});
-              $log.log('cc');
             }
             else {
               elem.css({'background-image':'url(' + images[4] + ')'});
-              $log.log('cc');
             }
           });
       }
     };
   }
 
-  mozartWelcomeDirective.$inject = ['$window', '$log'];
+  mozartWelcomeDirective.$inject = ['$window'];
 
   angular.module('mozArtApp')
     .directive('mzWelcome', mozartWelcomeDirective);
