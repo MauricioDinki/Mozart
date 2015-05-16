f6eb67b547fa55e08c16b140133b525d75539446
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2015-05-16 13:36:18 -0500

    Completed validation of the duration of an event

--- Events/forms.py
+++ Events/forms.py
@@ -33,24 +33,27 @@ class CreateEventForm(forms.ModelForm):
 	        self.fields[field].validators=[validate_blank]
 	        self.fields[field].required=True
 
-    def val_00(self, data):
+    def eval_00(self, data):
         if data == '00':
             return '24'
         else:
-            return '00'
+            return data
 
     def clean_finish_time(self):
         start_time  = self.cleaned_data.get('start_time')
     	finish_time = self.cleaned_data.get('finish_time')
 
-        ini = str(start_time).split(':')
-        fin = str(finish_time).split(':')
+        if start_time is not None:
+            ini = str(start_time).split(':')
+            fin = str(finish_time).split(':')
 
-        ini[0] = self.val_00(ini[0])
-        fin[0] = self.val_00(fin[0])
+            ini[0] = self.eval_00(ini[0])
+            fin[0] = self.eval_00(fin[0])
 
-        print ini[0]
-        print fin[0]
+            min_fin = (int(fin[0])*60) + int(fin[1])
+            min_ini = (int(ini[0])*60) + int(ini[1])
 
-
+            if abs(min_fin - min_ini) < 30:
+                raise forms.ValidationError('El evento no puede ser tan corto')     
+        
         return start_time and finish_time
