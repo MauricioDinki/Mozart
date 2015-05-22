9e48bfc0aa3bba3ab9d3d4474564d19dfc9fc9ea
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2015-05-21 22:16:16 -0500

    lint events module

--- Events/forms.py
+++ Events/forms.py
@@ -6,30 +6,28 @@ from django.utils.text import slugify
 from Utils.validations import default_messages, custom_messages, eval_blank, eval_iexact
 import time
 
+
 class CreateEventForm(forms.ModelForm):
-    '''
-        This form is a ModelForm that create events
-    '''
     class Meta:
         model = Event
-        exclude = ['user','slug']
+        exclude = ['user', 'slug']
         widgets = {
-        	'name': forms.TextInput(
-        		attrs = {
-        			'class': ''
-        		}
-    		),
+            'name': forms.TextInput(
+                attrs={
+                    'class': ''
+                }
+            ),
             'description': forms.Textarea(
-            	attrs = {
-            		'cols': 40, 
-            		'rows': 10,
-        		}
-    		),
-    		'date': forms.TextInput(
-    			attrs = {
-    				'type' : 'date',
-    			}
-			)
+                attrs={
+                    'cols': 40,
+                    'rows': 10,
+                }
+            ),
+            'date': forms.TextInput(
+                attrs={
+                    'type': 'date',
+                }
+            )
         }
 
     def __init__(self, *args, **kwargs):
@@ -37,8 +35,8 @@ class CreateEventForm(forms.ModelForm):
         super(CreateEventForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].error_messages.update(default_messages)
-            self.fields[field].validators=[eval_blank]
-            self.fields[field].required=True
+            self.fields[field].validators = [eval_blank]
+            self.fields[field].required = True
 
     def eval_00(self, data):
         if data == '00':
@@ -61,8 +59,8 @@ class CreateEventForm(forms.ModelForm):
         return event_date
 
     def clean_finish_time(self):
-        start_time  = self.cleaned_data.get('start_time')
-    	finish_time = self.cleaned_data.get('finish_time')
+        start_time = self.cleaned_data.get('start_time')
+        finish_time = self.cleaned_data.get('finish_time')
 
         if start_time is not None:
             ini = str(start_time).split(':')
@@ -75,31 +73,25 @@ class CreateEventForm(forms.ModelForm):
             min_ini = (int(ini[0])*60) + int(ini[1])
 
             if abs(min_fin - min_ini) < 30:
-                raise forms.ValidationError(custom_messages['shevent'])     
-        
+                raise forms.ValidationError(custom_messages['shevent'])
+
         return start_time and finish_time
 
     def clean_name(self):
         data = self.cleaned_data.get('name')
-        return eval_iexact(data, self.Meta.model,'slug')
+        return eval_iexact(data, self.Meta.model, 'slug')
 
     def save(self):
         cleaned_data = super(CreateEventForm, self).clean()
         newEvent = Event(
-            user = self.request.user,
-            name = cleaned_data.get('name'),
-            cover = cleaned_data.get('cover'),
-            date = cleaned_data.get('date'),
-            description = cleaned_data.get('description'),
-            finish_time = cleaned_data.get('finish_time'),
-            start_time = cleaned_data.get('start_time'),
-            place = cleaned_data.get('place'),
-            slug = slugify(cleaned_data.get('name')),
+            user=self.request.user,
+            name=cleaned_data.get('name'),
+            cover=cleaned_data.get('cover'),
+            date=cleaned_data.get('date'),
+            description=cleaned_data.get('description'),
+            finish_time=cleaned_data.get('finish_time'),
+            start_time=cleaned_data.get('start_time'),
+            place=cleaned_data.get('place'),
+            slug=slugify(cleaned_data.get('name')),
         )
         newEvent.save()
-
-
-
-
-        
-
--- Events/models.py
+++ Events/models.py
@@ -1,60 +1,60 @@
+# -*- encoding: utf-8 -*-
+
 from django.db import models
-from Profiles.models import days,months
 from django.utils.text import slugify
 from django.contrib.auth.models import User
 
-def event_cover_url(self,filename):
-	return str('event-cover/%s/%s')%(self.user.username,filename)
+
+def event_cover_url(self, filename):
+    return str('event-cover/%s/%s') % (self.user.username, filename)
+
 
 class Event(models.Model):
-	"""
-		Database model for events
-	"""
-	cover = models.FileField(
-		blank=True,
-		null=True,
-		upload_to=event_cover_url
-	)
-	date = models.DateField(
-		blank=True,
-		null=True,
-	)
-	description = models.CharField(
-		blank=True,
-		max_length=200,
-		null=True,
-	)
-	finish_time = models.TimeField(
-		blank=True,
-		null=True,
-	)
-	name = models.CharField(
-		blank=True,
-		max_length=50,
-		null=True,
+    cover = models.FileField(
+        blank=True,
+        null=True,
+        upload_to=event_cover_url
+    )
+    date = models.DateField(
+        blank=True,
+        null=True,
+    )
+    description = models.CharField(
+        blank=True,
+        max_length=200,
+        null=True,
+    )
+    finish_time = models.TimeField(
+        blank=True,
+        null=True,
+    )
+    name = models.CharField(
+        blank=True,
+        max_length=50,
+        null=True,
 
-	)
-	place = models.CharField(
-		blank=True,
-		max_length=50,
-		null=True,
-	)
-	slug = models.SlugField(
-		max_length=50,
-		unique=True,
-	)
-	start_time = models.TimeField(
-		blank=True,
-		null=True,
-	)
-	user = models.ForeignKey(
-		User,
-		null=True,
-	)
+    )
+    place = models.CharField(
+        blank=True,
+        max_length=50,
+        null=True,
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+    )
+    start_time = models.TimeField(
+        blank=True,
+        null=True,
+    )
+    user = models.ForeignKey(
+        User,
+        null=True,
+    )
 
-	class Meta:
-		verbose_name = "Event"
-		verbose_name_plural = "Events"
+    class Meta:
+        verbose_name = "Event"
+        verbose_name_plural = "Events"
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
